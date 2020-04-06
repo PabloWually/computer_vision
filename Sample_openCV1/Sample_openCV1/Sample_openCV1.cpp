c7ccab6eee6aaf11b7ce30ae961eb0cf93b5d8b6
Pablo Guallichico <pablomp250@gmail.com>
2020-04-06 15:21:55 -0500

    Rectangle
    Draw a rectangle on an imagen using the fuction "Mat::at" of opencv

--- Sample_openCV1/Sample_openCV1/Sample_openCV1.cpp
+++ Sample_openCV1/Sample_openCV1/Sample_openCV1.cpp
@@ -1,4 +1,3 @@
-
 #include "opencv2/opencv.hpp"
 #include <iostream>
 
@@ -10,20 +9,29 @@ int main()
 	//Nombre de la imagen que se va a cargar
 	char NombreImagen[] = "ivvi.jpg";
 	Mat imagen;
+	int i, j;
 
 	//Cargamos la imagen y se comprueba que lo ha hecho correctamente
-	imagen = imread(NombreImagen);
+	imagen = cv::imread(NombreImagen);
 	if (!imagen.data) {
 		cout << "Error al cargar la imagen: " << NombreImagen << endl;
 		exit(1);
+	}
+
+	for (i = imagen.rows / 4; i < 3 * imagen.rows / 4; ++i) {
+		for (j = imagen.cols / 4; j < 3 * imagen.cols / 4; ++j) {
+			imagen.at<Vec3b>(Point(j, i)) = Vec3b(0, 0, 0);
+		}
 	}
 
 	//Mostrar la imagen
 	namedWindow("Original", WINDOW_AUTOSIZE);
 	imshow("Original", imagen);
 
-	//Esperar a pulsar una tecla
-	waitKey(0);
+	//Guardar el resultado
+	imwrite("Resultado.jpg", imagen);
 
+	//Esperar a pulsar una tecla
+	cv::waitKey(0);
 	return 0;
 }
